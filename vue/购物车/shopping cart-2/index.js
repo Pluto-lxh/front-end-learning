01416d974b314bde5dcf5f84aa21c4e15a36d3f5
xiaohua <Galaxy__lxh@163.com>
2019-09-21 23:26:18 +0800

    version2
    增加是否选中商品的功能，增加全选按钮，只计算选中商品的价格

--- vue/购物车/shopping cart-2/index.js
+++ vue/购物车/shopping cart-2/index.js
@@ -6,29 +6,38 @@ var app=new Vue({
                id:1,
                name:"HUAWEI Mate30",
                price:4999,
-               count:1
+               count:1,
+               check:false
            },
            {
                 id:1,
                 name:"iPad 2018",
                 price:2999,
-                count:1
+                count:1,
+                check:false
             },
             {
                 id:1,
                 name:"HUAWEI P20",
                 price:3500,
-                count:1
+                count:1,
+                check:false
             },
-        ]
+        ],
+        checked:false
          
     },
     computed:{
         totalPrice:function(){
             var total=0;
-            for(var i=0;i<this.list.length;i++){
+            var product=this.list;
+            for(var i=0;i<product.length;i++){
                 var item=this.list[i];
-                total+=item.price*item.count;
+                //只计算选中商品的总价
+                if(product[i].check){
+                    total+=item.price*item.count;
+                }
+                
             }
             return total;
         }
@@ -43,6 +52,22 @@ var app=new Vue({
         },
         remove:function(index){
             this.list.splice(index,1);
+        },
+        AllChecked:function(){
+            if(this.checked){
+                console.log("全选中了")
+                var arr=this.list;
+                for(var i=0;i<arr.length;i++){
+                    //console.log(arr[i]);
+                    arr[i].check=true;
+                }
+            }else{
+                console.log("全没选中")
+                var arr=this.list;
+                for(var i=0;i<arr.length;i++){
+                    arr[i].check=false;
+                }
+            }
         }
     }
 
